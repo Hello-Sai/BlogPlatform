133087fb6c713b7f898707de7dfb8d49bbd22b48
Sai Kumar <dagudusai@gmail.com>
2024-07-13 20:46:24 +0530

    Task3 Updated

--- BlogPlatform/settings.py
+++ BlogPlatform/settings.py
@@ -122,6 +122,7 @@ STATIC_URL = "static/"
 STATIC_FILE_DIRS = [
     BASE_DIR / 'blog/templates/'
 ]
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
